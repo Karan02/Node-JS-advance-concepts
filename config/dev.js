f8cf330982ba40347d25a7c8aee084f1ff140acf
Karan02 <krnptl1234@gmail.com>
2020-07-04 11:50:15 +0530

    done

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://karan:<password>@cluster0-shard-00-00-tt6mn.mongodb.net:27017,cluster0-shard-00-01-tt6mn.mongodb.net:27017,cluster0-shard-00-02-tt6mn.mongodb.net:27017/advance-node?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+    '766269382776-1sv5c9fiju1cukqigbapb1ko4s8jnvfp.apps.googleusercontent.com',
+  googleClientSecret: '4svkxAWU7sWS91DkjKwTm1lz',
+  mongoURI: 'mongodb://karan:karan@cluster0-shard-00-00-tt6mn.mongodb.net:27017,cluster0-shard-00-01-tt6mn.mongodb.net:27017,cluster0-shard-00-02-tt6mn.mongodb.net:27017/advance_node?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
